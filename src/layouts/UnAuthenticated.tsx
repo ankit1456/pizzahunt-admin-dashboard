ca242ae78a09b5b9c1b689fe9e2c540f51dc7e86
ankit tripathi <ankittripathi1456@gmail.com>
2024-01-24 01:46:34 +0530

    create app layout

---
+++ src/layouts/UnAuthenticated.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+const UnAuthenticated = () => {
+  return <Outlet />;
+};
+
+export default UnAuthenticated;
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Login from "./pages/login/Login";
+import Dashboard from "./layouts/Dashboard";
+import UnAuthenticated from "./layouts/UnAuthenticated";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <Dashboard />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+    ],
   },
   {
-    path: "/auth/login",
-    element: <Login />,
+    path: "/auth",
+    element: <UnAuthenticated />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
